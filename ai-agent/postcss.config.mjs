7988b08c008aae2cead11bf5fc7f9c5b5de8bfc4
AlainDLC <jd_alain@hotmail.com>
2025-04-28 15:15:36 +0200

    change prop

--- ai-agent/postcss.config.mjs
+++ ai-agent/postcss.config.mjs
@@ -1,5 +1,11 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2323FF", // Här anger du din primära färg
+      },
+    },
+  },
 };
-
 export default config;
