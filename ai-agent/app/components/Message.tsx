65c606e9eed7433f9a4cc635f2e895e695b59396
AlainDLC <jd_alain@hotmail.com>
2025-05-17 11:21:05 +0200

    css done

--- ai-agent/app/components/Message.tsx
+++ ai-agent/app/components/Message.tsx
@@ -1,15 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import { Message } from "@/types/types";
 import { UserCircle } from "lucide-react";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
+import { useEffect, useRef } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 function Messages({ messages }: { messages: Message[] }) {
+  const ref = useRef<HTMLDivElement>(null);
   const path = usePathname();
-
   const isReviewsPage = path.includes("review-sessions");
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
 
   return (
     <div className="flex-1 flex flex-col overflow-y-auto space-y-10 py-10 px-5 bg-white rounded-lg">
@@ -41,13 +49,66 @@ function Messages({ messages }: { messages: Message[] }) {
                   : "chat-bubble-secondary bg-green-200 "
               }`}
             >
-              <ReactMarkdown rehypePlugins={[remarkGfm]}>
+              <ReactMarkdown
+                rehypePlugins={[remarkGfm]}
+                components={{
+                  ul: ({ node, ...props }) => (
+                    <ul
+                      {...props}
+                      className="list-disc list-inside ml-5 mb-5"
+                    />
+                  ),
+
+                  ol: ({ node, ...props }) => (
+                    <ul
+                      {...props}
+                      className="list-decimal list-inside ml-5 mb-5"
+                    />
+                  ),
+                  h1: ({ node, ...props }) => (
+                    <h1 {...props} className="text-2xl font-bold ml-5 mb-5" />
+                  ),
+                  h2: ({ node, ...props }) => (
+                    <h2 {...props} className="text-xl font-bold ml-5 mb-5" />
+                  ),
+                  h3: ({ node, ...props }) => (
+                    <h3 {...props} className="text-lg font-bold ml-5 mb-5" />
+                  ),
+                  table: ({ node, ...props }) => (
+                    <table
+                      {...props}
+                      className="table-auto w-full border-separate border-2 rounded-sm border-spacing-4 border-white ml-5 mb-5"
+                    />
+                  ),
+                  th: ({ node, ...props }) => (
+                    <th {...props} className="text-left underline" />
+                  ),
+                  p: ({ node, ...props }) => (
+                    <p
+                      {...props}
+                      className={`whitespace-break-spaces mb-5 ${
+                        message.content === "Thinking..." && "animate-pulse"
+                      }
+                    ${isSender ? "text-white" : "text-gray-400"}`}
+                    />
+                  ),
+                  a: ({ node, ...props }) => (
+                    <a
+                      {...props}
+                      target="_blank"
+                      className="font-bolde underline hover:text-blue-300"
+                      rel="noopener norferrer"
+                    />
+                  ),
+                }}
+              >
                 {message?.content || "KUNG ÄR DU ALAIN"}
               </ReactMarkdown>
             </p>
           </div>
         );
       })}
+      <div ref={ref} />
     </div>
   );
 }
